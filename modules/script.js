c6d2dc937cf3c9dafcc68543fce0173e9c1c9051
ide-k <siddharthkhanna2k@gmail.com>
2022-03-03 17:47:45 -0500

    initial

---
+++ modules/script.js
@@ -0,0 +1,11 @@
+//import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+import * as THREE from "three";
+
+//import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export const createThree = () => {
+
+    
+
+};
